d8cde24f533153ec78e10c0ce864f55b51b7bd9b
Miranda Begashvili <begashvilimira@gmail.com>
2023-03-03 16:01:33 +0400

    Jenkins Project

---
+++ Mock/src/test/java/TestSth.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class TestSth {
+
+    @Test
+    public  void sth () {
+        System.out.println("Hello world!");
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
